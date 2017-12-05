c96f7637c7e7f074d4dcb71b8b571b77b8be8038
Katu_brd <katuthings@gmail.com>
2017-12-05 11:11:34 +0100

    new layout - header

--- js/main.js
+++ js/main.js
@@ -1,25 +1,77 @@
+  // var img1 = {url: 'assets/header/bg_plant.svg', x: 5, y: 1, offset: -0.4},
+  //     img2 = {url: 'assets/header/flover.png', x: -10, y: -5, offset: 0.2},
+  //     img3 = {url: 'assets/header/border.svg', x: 5, y: 10, offset: -0.7};
 
-//image-slider
-var imageArray = ["assets/img/1.jpg", "assets/img/3.jpg", "assets/img/5.jpg","assets/img/6.jpg","assets/img/7.jpg","assets/img/8.jpg"];
-var imageNumber = 0;
-var imageLength = imageArray.length -1;
-var time = 2300;
+  var img1 = document.getElementById('bg'),
+      img2 = document.getElementById('flover'),
+      img3 = document.getElementById('border');
 
-//function lineAnimation() {}
+  var assets = [
+    img1,
+    img2,
+    img3
+  ],
+    layers = [],
+    w = 600,
+    h = 800,
+    loaded = 0,
+    container = document.getElementById('header_parallax'),
+    s = new Snap(w, h),
+    c,
+    g;
 
-function changeImg(x) {
-  imageNumber +=x;
-  if (imageNumber > imageLength) {
-    imageNumber = 0;
+  container.appendChild(s.node);
+  TweenMax.set(s.node, {scale: 0.9});
+
+  g = s.g();
+  c = s.g();
+  g.append(c);
+
+  for (var i = 0; i < assets.length; i++) {
+    var img = new Image();
+    img.src = assets[i].url;
+    img.onload = handle_load;
+
+    var _img = s.image(assets[i].url, assets[i].x, assets[i].y);
+    c.append(_img);
+    layers.push(_img);
   }
-  if (imageNumber < 0) {
-    imageNumber = imageLength;
+
+  function handle_load() {
+    loaded += 1;
+
+    if (loaded == assets.length) {
+      handle_loaded();
+    }
   }
-  document.getElementById("mainImage").src = imageArray[imageNumber];
 
-  return false;
-}
+  function handle_loaded() {
+    container.addEventListener('mousemove', handle_mousemove);
+    container.addEventListener('mouseout', handle_mouseout);
+    container.addEventListener('mouseover', handle_mouseover);
+  }
 
-function autoRun() {
-  setInterval("changeImg(1)", 5000);
-}
+  function handle_mousemove(e) {
+    var dx = e.offsetX - (w / 2);
+    var dy = e.offsetY - (h / 2);
+
+    for (var i = 0; i < layers.length; i++) {
+      var l = layers[i];
+      var _x = dx * assets[i].offset;
+      var _y = dy * assets[i].offset;
+      TweenMax.to(l.node, 0.1, {x: _x, y: _y});
+    }
+  }
+
+  function handle_mouseout(e) {
+    for (var i = 0; i < layers.length; i++) {
+      var l = layers[i];
+      TweenMax.to(l.node, 0.2, {x: 0, y: 0, ease: Quad.easeOut});
+    }
+
+    TweenMax.to(s.node, 0.2, {scale: 0.9, ease: Quad.easeOut});
+  }
+
+  function handle_mouseover(e) {
+    TweenMax.to(s.node, 0.2, {scale: 1, ease: Back.easeOut});
+  }
